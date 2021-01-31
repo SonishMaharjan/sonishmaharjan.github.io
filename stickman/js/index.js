8aecfebc3067d7db569ec34cf35038d97af3bd4c
sonish <sonishmzn95@gmail.com>
2021-01-31 16:23:35 +0545

    frame addition added

--- stickman/js/index.js
+++ stickman/js/index.js
@@ -3,8 +3,28 @@ let frameManager = {
   activeFrame: null,
 
   addFrame() {
-    this.framesList.push(new Frame("frame-" + this.framesList.length));
-    this.activeFrame = this.framesList[this.framesList.length - 1];
+    if (this.activeFrame) {
+      let activeFrameIndex = this.framesList.findIndex(
+        (frame) => frame.id === this.activeFrame.id
+      );
+
+      // console.log("hahah");
+      let startArray = this.framesList.slice(0, activeFrameIndex + 1);
+      let endArray = this.framesList.slice(
+        activeFrameIndex + 1,
+        this.framesList.length
+      );
+
+      startArray.push(new Frame("frame-" + this.framesList.length));
+
+      this.framesList = startArray.concat(endArray);
+
+      this.activeFrame = this.framesList[activeFrameIndex + 1];
+    } else {
+      this.framesList.push(new Frame("frame-" + this.framesList.length));
+      this.activeFrame = this.framesList[this.framesList.length - 1];
+    }
+
     this.render();
   },
 
