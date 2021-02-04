59c516e1e2d8b4f20910ab1f6df4094a3afee6df
sonish <sonishmzn95@gmail.com>
2021-02-04 17:26:15 +0545

    utils refactored

--- stickman/js/utils.js
+++ stickman/js/utils.js
@@ -5,26 +5,6 @@ function degToRad(deg) {
 function radToDeg(rad) {
   return (rad * 180) / Math.PI;
 }
-function getEndPoint(point, angle, length) {
-  let x = point.x + Math.cos(degToRad(angle)) * length;
-  let y = point.y + Math.sin(degToRad(angle)) * length;
-
-  return { x, y };
-}
-
-function getDistance(pointA, pointB) {
-  return Math.abs(
-    Math.sqrt(
-      Math.pow(pointA.x - pointB.x, 2) + Math.pow(pointA.y - pointB.y, 2)
-    )
-  );
-}
-
-function getAngle(p1, p2, origin) {
-  var d1 = this.getDistance(p1, origin);
-  var d2 = this.getDistance(p2, origin);
-  return Math.atan2(p2.y - p1.y, p2.x - p1.x);
-}
 
 function generateRandomCode(length) {
   let possible = "0123456789ABCD";
@@ -39,11 +19,4 @@ function generateRandomCode(length) {
 
 function randomInt(min, max) {
   return Math.random() * (max - min) + min;
-}
-
-function getCoordinates(origin, angle, distance) {
-  let x = origin.x + Math.cos(degToRad(angle)) * distance;
-  let y = origin.y + Math.sin(degToRad(angle)) * distance;
-
-  return { x: x, y: y };
 }
